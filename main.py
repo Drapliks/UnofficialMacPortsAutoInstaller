1c47a891f1765f189fe6e586729af4dcf611a79d
Drapliks <kinstevg@gmail.com>
2026-01-04 20:38:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def installmcports():
     os.system('make')
     os.system('make install')
 
-print('Welcome to Unofficial Auto MacPorts installer\n\nChoose your action:\n1.Install with xocde\n2.Install\n0.Exit')
+print('Welcome to Unofficial Auto MacPorts installer\n1.Install with xocde\n2.Install\n0.Exit')
 while True:
     act = input()
     if act == 1:
